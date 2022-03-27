1e2e8a4ce1ba49b6b46d4d07994d9aa3d42c63c0
unknown <amazingbhola@gmail.com>
2022-03-27 13:45:16 +0600

    webpack css configure by css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,20 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-module.exports = {
-    mode: "development",
+const config = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/main.js',
+        filename: 'app.js'
     },
     module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    MiniCssExtractPlugin.loader,
-
-                    // Translates CSS into CommonJS
-                    "css-loader",
-
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-        ],
+        rules: [{
+            test: /\.css$/,
+            use:[
+                'style-loader', 'css-loader'
+            ]
+        }]
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/main.css',
-        })
-    ],
     watch: true
-};
+}
+
+module.exports = config;
