677fc3d989360981cd72a85ead642febf45fcb82
unknown <amazingbhola@gmail.com>
2022-03-27 16:29:05 +0600

    sass file configure

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+
 const path = require('path');
 const config = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: 'js/app.js'
     },
     module: {
-        rules: [{
-            test: /\.css$/,
-            use:[
-                'style-loader', 'css-loader'
-            ]
-        }]
+        rules: [
+            {
+                test: /\.s[ac]ss/i,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename:'css/main-sass.css'
+        })
+    ],
     watch: true
 }
 
